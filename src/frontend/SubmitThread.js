ffd4dbf112b4bd3a9a5ae7910aee834c06d18c54
NilouferS <niloufer.shaikh11@gmail.com>
2019-07-04 14:17:06 +0100

    Task 6:  skeleton of the SubmitThread Page

--- src/frontend/SubmitThread.js
+++ src/frontend/SubmitThread.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-import './SubmitThread.css'
+import './SubmitThread.css';
 
 export default class SubmitThread extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            titleThread: '',
+            commentThread: ''
         };
     }
 
     render() {
-        return (<></>);
+        return (<div className='SubmitThread'>
+            <input type = 'text' placeholder = 'enter the title' value= {this.state.titleThread}></input>
+            <textarea placeholder = 'enter the comment' value = {this.state.commentThread}></textarea>
+            <button type ='submit' value = 'SUBMIT'></button>
+        </div>);
     }
 }
