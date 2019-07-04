7febfdf630b16b9cf0fe989da19baa83658befc0
Richard Lan <yiukencw@gmail.com>
2019-07-04 10:03:06 +0100

    Route the ForumIndex, ThreadPage and SubmitPages so that they are viewable, Create the stub files for each component, closes #1

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
+
+import ForumIndex from './ForumIndex';
+import SubmitThread from './SubmitThread';
+import Thread from './Thread';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        
+        <Route exact path='/' component={ForumIndex} />
+        <Route exact path='/index' component={ForumIndex} />
+        <Route exact path='/submit' component={SubmitThread} />
+        <Route exact path='/thread' component={Thread} />
       </Switch>
     </div>
   );
