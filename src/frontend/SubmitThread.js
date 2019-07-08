08264d43a4ae04d2586f9cf903980e3a7f1d5e58
Richard Lan <49878959+ykencw@users.noreply.github.com>
2019-07-08 16:41:20 +0100

    Fix issue with unhandled onChange events on Submit page, #6 (#26)
    requests compressed into one push request.

--- src/frontend/SubmitThread.js
+++ src/frontend/SubmitThread.js
@@ -6,16 +6,31 @@ export default class SubmitThread extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            titleThread: '',
-            commentThread: ''
+            threadTitle: '',
+            threadComment: ''
         };
+    }
+
+    textHandler = event => {
+        let value = event.target.value;
+        let name = event.target.name;
+        this.setState(() => ({[name]: value}));
     }
 
     render() {
         return (
             <div className='SubmitThread'>
-                <input className='item' type='text' placeholder='enter the title' value={this.state.titleThread}></input>
-                <textarea className='item' placeholder='enter the comment' value={this.state.commentThread}></textarea>
+                <input className='item' 
+                    name='threadTitle'
+                    type='text' 
+                    placeholder='enter the title'
+                    value={this.state.threadTitle}
+                    onChange={this.textHandler} />
+                <textarea className='item'
+                    name='threadComment'
+                    placeholder='enter the comment' 
+                    value={this.state.threadComment} 
+                    onChange={this.textHandler} />
                 <button className='item' type='submit' value='SUBMIT'>SUBMIT</button>
             </div>
         );
