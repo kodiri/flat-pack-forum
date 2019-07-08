6f2a78e182c40099c020caac125cf90acaac98e1
Abdul Bello <crownkunle@sikibell.onmicrosoft.com>
2019-07-08 07:18:42 +0100

    commit 1

--- src/frontend/Thread.js
+++ src/frontend/Thread.js
@@ -2,6 +2,43 @@ import React from 'react';
 
 import './Thread.css'
 
+class Post extends React.Component 
+{
+
+    render() {
+        return (
+            <div>
+                <UserDetails></UserDetails>
+                <Content />
+            </div>
+        );
+    }
+}
+
+class UserDetails extends React.Component
+{
+    render() {
+        return (
+            <div>
+                <p>user details</p>
+            </div>
+        );
+    }
+}
+
+class Content extends React.Component
+{
+    render() {
+        return (
+            <div>
+                <textarea></textarea>
+            </div>
+        );
+    }
+}
+
+
+
 export default class Thread extends React.Component {
     constructor(props) {
         super(props);
@@ -11,6 +48,14 @@ export default class Thread extends React.Component {
     }
 
     render() {
-        return (<></>);
+        return (
+            <div>
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </div>
+        );
     }
 }
