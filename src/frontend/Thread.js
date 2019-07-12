a6a2a6899144dc4f78eb7979dfc6147a834142b6
NilouferS <niloufer.shaikh11@gmail.com>
2019-07-12 15:01:44 +0100

    change the syle on pages

--- src/frontend/Thread.js
+++ src/frontend/Thread.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './Thread.css'
 
+export default class Thread extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [<Post key='1' user='UserName: alpha' content='post content1' />,
+            <Post key='2'user='UserName: brave' content='post content2' />,
+            <Post key='3'user='UserName: chally' content='post content3' />,
+            <Post key='4'user='UserName: delta' content='post content4' />,
+            <Post key='5'user='UserName: echo' content='post content5' />]
+        };
+    }
+
+    render() {
+        return (
+            <div className='Thread'>
+                {this.state.posts}
+            </div>
+        );
+    }
+}
+
 class Post extends React.Component {
     render() {
         return (
-            <div className = 'Post grid-container'>
-                <div className = 'user'><UserDetails user={this.props.user}/></div>
-                <div className = 'content'><Content content={this.props.content}/></div>              
+            <div className = 'Post'>
+                <UserDetails user={this.props.user}/>
+                <Content content={this.props.content}/>        
             </div>
         );
     }
@@ -27,27 +48,6 @@ class Content extends React.Component {
         return (
             <div className='Content'>
                 <p className='pUser'>{this.props.content}</p>
-            </div>
-        );
-    }
-}
-
-export default class Thread extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: [<Post key='1' user='UserName: alpha' content='post content1' />,
-            <Post key='2'user='UserName: brave' content='post content2' />,
-            <Post key='3'user='UserName: chally' content='post content3' />,
-            <Post key='4'user='UserName: delta' content='post content4' />,
-            <Post key='5'user='UserName: echo' content='post content5' />]
-        };
-    }
-
-    render() {
-        return (
-            <div className='Thread'>
-                {this.state.posts}
             </div>
         );
     }
---
+++ src/frontend/common/ToggleFn.js
@@ -0,0 +1,6 @@
+export default function Toggle(selector, className) {
+    console.log(selector);
+    const el = document.getElementById(selector)
+    console.log(el);
+        el.classList.toggle(className);
+}
