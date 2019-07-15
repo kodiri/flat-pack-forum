a68ba4110cb574bb66ef2e493289ca7eb85e19d6
Richard Lan <yiukencw@gmail.com>
2019-07-15 16:04:35 +0100

    Send post requests from frontend to backend with new Posts that are appended to a posts array on the backend and returned to the frontend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,62 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 const path = require('path');
 const buildPath = '../../build';
 const port = process.env.PORT || 3001;
+
+let posts = [{  // Backend's TEMPORARY array of Thread posts
+    user: {
+        username: 'Peter7737'
+    },
+    content: 'Yesterday was a beautiful Sunny day, tommorrow may be ' + 
+        'even better!'
+},
+{
+    user: {
+        username: 'Infelible_Ireden'
+    },
+    content: 'wat'
+},
+{
+    user: {
+        username: 'Peter7737'
+    },
+    content: 'Good day to you, are you enjoying the Sun\'s brilliant' +
+        ' rays today?'
+},
+{
+    user: {
+        username: 'xZeronLegends_00x'
+    },
+    content: 'Ignore Peter7737, he is a bot, please report this thread'
+},
+{
+    user: {
+        username: 'domdanouseWalker'
+    },
+    content: 'i think peter is actually a real person'
+},
+{
+    user: {
+        username: 'Peter7737'
+    },
+    content: 'Come my Sun brothers, there is plenty of Sun for ' + 
+        'everyone to praise!'
+},
+{
+    user: {
+        username: 'Admin'
+    },
+    content: 'That\'s enough Peter, I\'m closing this thread'
+},
+{
+    user: {
+        username: 'xZeronLegends_00x'
+    },
+    content: 'FINALLY'
+}];
 
 app.get('/refresh-session', (_req, res) => {
     res.end(JSON.stringify({
@@ -48,62 +102,10 @@ app.get('/rest/threads', (_req, res) => {
 });
 
 app.get('/rest/posts/:thread', (_req, res) => {
-    res.end(JSON.stringify([
-        {
-            user: {
-                username: 'Peter7737'
-            },
-            content: 'Yesterday was a beautiful Sunny day, tommorrow may be ' + 
-                'even better!'
-        },
-        {
-            user: {
-                username: 'Infelible_Ireden'
-            },
-            content: 'wat'
-        },
-        {
-            user: {
-                username: 'Peter7737'
-            },
-            content: 'Good day to you, are you enjoying the Sun\'s brilliant' +
-                ' rays today?'
-        },
-        {
-            user: {
-                username: 'xZeronLegends_00x'
-            },
-            content: 'Ignore Peter7737, he is a bot, please report this thread'
-        },
-        {
-            user: {
-                username: 'domdanouseWalker'
-            },
-            content: 'i think peter is actually a real person'
-        },
-        {
-            user: {
-                username: 'Peter7737'
-            },
-            content: 'Come my Sun brothers, there is plenty of Sun for ' + 
-                'everyone to praise!'
-        },
-        {
-            user: {
-                username: 'Admin'
-            },
-            content: 'That\'s enough Peter, I\'m closing this thread'
-        },
-        {
-            user: {
-                username: 'xZeronLegends_00x'
-            },
-            content: 'FINALLY'
-        },
-    ]));
+    res.end(JSON.stringify(posts));
 })
 
-app.post('/rest/submit-thread', (_req, res) => {
+app.post('/rest/submit-thread', jsonParser, (_req, res) => {
     console.log("Submit Thread request received");
     res.end(JSON.stringify({
         result: true,
@@ -111,12 +113,21 @@ app.post('/rest/submit-thread', (_req, res) => {
     }))
 });
 
-app.post('/rest/submit-post', (_req, res) => {
+app.post('/rest/submit-post', jsonParser, (req, res) => {
     console.log("Submit Post request received");
+    const post = req.body.post;
+    posts.push({
+        user: {
+            username: 'Guest'
+        },
+        content: post
+    });
     res.end(JSON.stringify({
         result: true,
-        message: "Successfully submitted post to the backend!"
-    }))
+        message: "Successfully submitted post to the backend!",
+        post: `The post: ${post}`,
+        posts: posts
+    }));
 });
 
 app.use(express.static(path.join(__dirname, buildPath)));
