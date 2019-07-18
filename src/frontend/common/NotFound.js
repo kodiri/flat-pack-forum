407b87049c8a1573b0925d7e190a569d65a6e554
Richard Lan <yiukencw@gmail.com>
2019-07-18 16:11:30 +0100

    Fix NotFound Page, closes #64

--- src/frontend/common/NotFound.js
+++ src/frontend/common/NotFound.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function NotFound() {
-    return (<>
+    return (<div id='NotFound'>
         <h1>404, Page Not Found!</h1>
         <Link to='/index'>Click here to go back! :)</Link>
-    </>);
+    </div>);
 }
