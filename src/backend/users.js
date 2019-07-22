5271cc233b1d47208e4b6d75ea60693d94387194
Richard Lan <yiukencw@gmail.com>
2019-07-22 12:33:35 +0100

    Create and serve placeholder users from backend

---
+++ src/backend/users.js
@@ -0,0 +1,79 @@
+module.exports = function getUsers() {  // Backend's TEMPORARY array of Users
+    return [
+        {
+            username: 'RoAvamuri',
+            uuid: 0,
+            location: 'USA'
+        },
+        {
+            username: 'xZeronLegends_00x',
+            uuid: 1,
+            location: 'USA'
+        },
+        {
+            username: 'MSarah2',
+            uuid: 2,
+            location: 'UK'
+        },
+        {
+            username: '200JAIN',
+            uuid: 3,
+            location: 'Denmark'
+        },
+        {
+            username: 'markMort',
+            uuid: 4,
+            location: 'Norway'
+        },
+        {
+            username: 'Anuenooru',
+            uuid: 5,
+            location: 'France'
+        },
+        {
+            username: 'domdanouseWalker',
+            uuid: 6,
+            location: 'Germany'
+        },
+        {
+            username: 'BarryA',
+            uuid: 7,
+            location: 'UK'
+        },
+        {
+            username: 'Patadada',
+            uuid: 8,
+            location: 'Portugal'
+        },
+        {
+            username: '銹嫺',
+            uuid: 9,
+            location: 'China'
+        },
+        {
+            username: 'Peter7737',
+            uuid: 10,
+            location: 'Italy'
+        },
+        {
+            username: 'Infelible_Ireden',
+            uuid: 11,
+            location: 'Romania'
+        },
+        {
+            username: 'Admin',
+            uuid: 12,
+            location: 'UK'
+        },
+        {
+            username: 'Victor',
+            uuid: 13,
+            location: 'Poland'
+        },
+        {
+            username: 'БОУЦЛѢ',
+            uuid: 14,
+            location: 'Russia'
+        },
+    ]
+}
