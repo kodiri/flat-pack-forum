8d0370dede2d61a52a4051d4049f2133505b2ed7
NilouferS <niloufer.shaikh11@gmail.com>
2019-07-25 10:50:14 +0100

    Populatted the UserProfile with data from backend #84

--- src/frontend/UserProfile.js
+++ src/frontend/UserProfile.js
@@ -3,14 +3,63 @@ import './UserProfile.css';
 import avatar from './common/images/avatar.png';
 
 export default class UserProfile extends React.Component {
-    render(){
-        return(
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            location: '',
+            lastActive:'',
+            joinDate: '',
+            extra: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch(`/rest/user/${this.props.match.params.number}`).then(res => {
+            return res.ok ? res.json() : Promise.reject();
+        }).then(res => {
+            console.log(res);
+            if (res.hasOwnProperty('username')) {
+                this.setState({ username: res.username, location: res.location, lastActive: res.lastActive, joinDate: res.joinDate  });
+            } else {
+                this.setState({ username: undefined });
+            }
+        });
+    }
+
+    render() {
+        const { username, location, joinDate, lastActive } = this.state;
+        return (
             <div id='UserProfile'>
-                <img className='avatar'src={avatar} alt='avatar'/>
-                <div className='username'>PETER</div>
-                <div className='extra'>extra details</div>
-                <div className='about'>blah blahhhhhhhhhhhhhh</div>
+                <img className='avatar' src={avatar} alt='avatar' />
+                <div className='username'>{username}</div>
+                <div className='extra'>
+                    Join Date: {
+                        new Date(joinDate).toLocaleDateString(
+                            "en-UK",
+                            {
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric',
+                            })
+                    }
+                    <br/>
+                    Last Active: {
+                        new Date(lastActive).toLocaleDateString(
+                            "en-UK",
+                            {
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric',
+                            })
+                        }
+                    <br/>
+                    Country: {location}
+                </div>
+                <div className='about'/>
             </div>
         );
     }
 }
+
+
