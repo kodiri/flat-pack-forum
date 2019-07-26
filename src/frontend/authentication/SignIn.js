a4f1a0e7b93e0e3676738d0deb0bba0cb0642932
NilouferS <niloufer.shaikh11@gmail.com>
2019-07-26 14:13:48 +0100

    Unifying SignUp and SignIn page

--- src/frontend/authentication/SignIn.js
+++ src/frontend/authentication/SignIn.js
@@ -6,23 +6,31 @@ import './SignIn.css';
 
 class SignIn extends React.Component {
     render() {
-        return (<div className='SignIn'>
-            <div className='title'>Welcome to the Sign In page!</div>
-            <div>Please enter your details here:</div>
-            <label htmlFor='username'>Username:</label>
-            <input name='username' type='text' />
-            <label htmlFor='password'>Password:</label>
-            <input name='password' type='password' />
-
-            <div>OR</div>
-            <GoogleSignIn
-                clientId='407818662698-mdsp622g5v0hmi7dsdqp2drvraebgnj4.apps.googleusercontent.com'
-                buttonText='Sign In with Google!'
-                onSuccess={res => handleGoogleSignIn(
-                    res, this.props.history, this.props.refreshSession)}
-                onFailure={handleGoogleFailure} />
-        </div>);
-    }
-}
-
+        return (
+            <div className='SignIn'>
+                <h1>Welcome to the Sign In page!</h1>
+                <div className='ContainerSignIn'>
+                    <label htmlFor='email'>Email:</label>
+                    <input name='email' type='email' />
+                    <label htmlFor='password'>Password:</label>
+                    <input name='password' type='password' />
+                </div>
+                <button
+                    className='submitButton'
+                    type='Submit'>
+                    Submit
+                </button>
+                    <br />
+                    <div className='line'></div>
+                    <GoogleSignIn
+                        className='googleSignIn'
+                        clientId='407818662698-mdsp622g5v0hmi7dsdqp2drvraebgnj4.apps.googleusercontent.com'
+                        buttonText='Sign In with Google!'
+                        onSuccess={res => handleGoogleSignIn(
+                            res, this.props.history, this.props.refreshSession)}
+                        onFailure={handleGoogleFailure} />
+                </div>);
+            }
+        }
+        
 export default SignIn;
